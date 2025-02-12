44e1337710774c93168e71911bf32730bce3325a
Mike <mike.szklarz@gmail.com>
2025-02-12 17:51:15 -0500

    Updated bash script to allow changing of flags

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -3,16 +3,17 @@
 #SBATCH --job-name=ddp-test-%j         # name of the job
 #SBATCH --output=logs/job_%j.txt       # output log file. MAKE SURE 'logs' DIRECTORY EXISTS OR ELSE THIS WILL ERROR 
 
-#SBATCH --nodes=2                      # number of nodes, can be changed with -N flag
+#SBATCH --nodes=1                      # number of nodes, can be changed with -N flag
 #SBATCH --ntasks-per-node=1            # number of tasks per node. We only have 1 gpu per node. Dont change this
 
-#SBATCH --mem=65GB                     # memory per node
+#SBATCH --mem=${MEMORY:-65GB}          # memory per node
 #SBATCH --partition=waccamaw           # partition name
 #SBATCH --time=01:00:00                # time limit hrs:min:sec
 #SBATCH --exclusive                    # exclusive use of node resoureces. Not sure if this works slurm 16.05.9
 
 # Can exclude nodes with "#SBATCH --exclude=waccamaw03, waccamaw04"
 # For current testing not needed.
+
 
 nodes=( $(scontrol show hostnames $SLURM_JOB_NODELIST) )
 nodes_array=(${nodes[@]})
@@ -25,16 +26,22 @@ echo Node IP: $head_node_ip
 export LOGLEVEL=INFO
 
 # used for debugging purposes
-export NCCL_DEBUG=INFO
+# export NCCL_DEBUG=INFO
 
 # needed or else the NCCL socket uses the wrong interface
 # can also use NCCL_COMM_ID=<comm_id>:<port> but this is easier
 export NCCL_SOCKET_IFNAME=eno8303
 
+NNODES=${NNODES:-$NODES}            # default to SLURM node count 1
+RDZV_PORT=${RDZV_PORT:-29500}       # default rendezvous port 29500
+
+source /mnt/cidstore1/software/debian12/anaconda3/etc/profile.d/conda.sh
+conda activate ddp-test
+
 srun torchrun \
---nnodes 2 \
+--nnodes $SLURM_NNODES \
 --nproc_per_node 1 \
 --rdzv_id $RANDOM \
 --rdzv_backend c10d \
---rdzv_endpoint $head_node_ip:29500 \
+--rdzv_endpoint $head_node_ip:$RDZV_PORT \
 ./src/multinode.py 50 10
